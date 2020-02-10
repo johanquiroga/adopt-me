56383e8671fcabbf64e631ffebd84d9c6875729d
Johan Quiroga <johan.c.quiroga@gmail.com>
2020-02-10 16:46:01 -0500

    Added emotion and some basic styles

--- src/Details.js
+++ src/Details.js
@@ -58,7 +58,12 @@ class Details extends React.Component {
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
           <ThemeContext.Consumer>
             {([theme]) => (
-              <button onClick={this.toggleModal} style={{ backgroundColor: theme }}>Adopt {name}</button>
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: theme }}
+              >
+                Adopt {name}
+              </button>
             )}
           </ThemeContext.Consumer>
           <p>{description}</p>
